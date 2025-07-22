b13367b603b6489e2d2e2f66e390efd9ec054aab
bhavika08715 <bhavikayalamanchili@gmail.com>
2025-07-22 14:53:00 +0530

    Add files via upload

---
+++ Geeks.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Geeks
+ { 
+public static void main(String[] args) 
+{
+ int i = 10;
+ // Wideing TypeCasting (Automatic Casting)
+ // from int to long 
+long l = i;
+ // Wideing TypeCasting (Automatic Casting)
+ // from int to double
+ double d = i;
+ System.out.println("Integer: " + i); 
+System.out.println("Long: " + l); 
+System.out.println("Double: " + d); 
+}
+}
